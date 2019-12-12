c88dca7b265fad33a16f0909c13c82e9b9de62cb
Joel Weiser <joel.weiser@oicr.on.ca>
2019-12-12 16:50:58 -0500

    creates and uses a functional component

---
+++ app/src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>My Name</h1>
+            <p>Vacation spots to visit</p>
+            <ul>
+                <li>Spot 1</li>
+                <li>Spot 2</li>
+                <li>Spot 3</li>
+            </ul>
+        </div>
+    );
+}
+
+export default MyInfo;
