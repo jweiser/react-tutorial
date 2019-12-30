0a80030f301fc0ba8a1ec397c9120b30596849c6
Joel Weiser <joel.weiser@oicr.on.ca>
2019-12-30 15:22:15 -0500

    useEffect hook lesson to change the color of the count state

--- app/src/components/lessons/hooks/App.js
+++ app/src/components/lessons/hooks/App.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import randomcolor from 'randomcolor';
 
 function App() {
     const [count, setCount] = useState(0);
+    const [color, setColor] = useState('');
 
     function increment() {
         setCount(prevCount => prevCount + 1);
@@ -11,9 +13,15 @@ function App() {
         setCount(prevCount => prevCount - 1);
     }
 
+    // Array as the second parameter is the list of state variables to watch for changes for re-running the effect
+    // (an empty array is the same as running the effect only once as it would if using the componentDidMount method)
+    useEffect(() => {
+        setColor(randomcolor());
+    }, [count]);
+
     return (
         <div>
-            <h1>{count}</h1>
+            <h1 style={{color: color}}>{count}</h1>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
         </div>
