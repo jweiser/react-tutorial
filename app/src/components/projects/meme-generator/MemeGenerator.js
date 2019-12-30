5421b7e40f1ec478a93604a7dc7475a337672be1
Joel Weiser <joel.weiser@oicr.on.ca>
2019-12-30 13:45:32 -0500

    creates starting files for MemeGenerator project

---
+++ app/src/components/projects/meme-generator/MemeGenerator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class MemeGenerator extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <h1>Meme Generator Section</h1>
+        );
+    }
+}
+
+export default MemeGenerator;
